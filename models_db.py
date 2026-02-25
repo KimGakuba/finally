a277935a3eec669e6bc235f5352f29fef359792f
NGABONZIZA Kim Gakuba <kimgakuban@gmail.com>
2026-02-25 07:38:02 +0300

    Update models_db.py

--- models_db.py
+++ models_db.py
@@ -1,21 +1,18 @@
-from sqlalchemy import Column, Integer, String, Float, DateTime
+from sqlalchemy import Column, Integer, String, Float, DateTime, ForeignKey
 from datetime import datetime
 from database import Base
 
-
 class User(Base):
     __tablename__ = "users"
-
     id = Column(Integer, primary_key=True)
-    username = Column(String(100), unique=True)
-    password = Column(String(200))
-
+    username = Column(String(100), unique=True, nullable=False)
+    password = Column(String(200), nullable=False)
 
 class PredictionLog(Base):
     __tablename__ = "prediction_logs"
-
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer)
+    user_id = Column(Integer, ForeignKey("users.id"))
     prediction = Column(String(50))
     confidence = Column(Float)
+    recommendation = Column(String(200))
     timestamp = Column(DateTime, default=datetime.utcnow)
